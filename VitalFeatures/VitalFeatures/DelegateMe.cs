5d65bd15fdc8fe432eb98063d94eb92f745f2b24
Arvind2y <arvind2y@gmail.com>
2017-07-02 11:17:06 +0530

    Vital fetaures like delegate, events and many more code added

---
+++ VitalFeatures/VitalFeatures/DelegateMe.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VitalFeatures
+{
+
+    public class DelegateMe
+    {
+
+        // Delegate without parameter without any return value
+        Action action = null;
+
+        Func<string> delegateReturns = null;
+
+
+        public DelegateMe()
+        {
+            action = () => { ShowToConsole(); ShowToWindow(); };
+            action += ShowToConsole;
+            action += ShowToWindow;
+        }
+
+
+        public void BindMethodToDelegate()
+        {
+            action();
+            delegateReturns = () => ReturnsToWindow();
+            //delegateReturns();
+            Console.WriteLine(delegateReturns());
+        }
+
+        private string ShowToWindow(string message)
+        {
+            Console.WriteLine($"ShowtoWindow method called.{message}");
+            return $"You have entered {message} ";
+        }
+        private string ReturnsToWindow()
+        {
+            //Console.WriteLine($"ShowtoWindow method called.{message}");
+            return $"You have returend from a delegate.";
+        }
+        private void ShowToWindow()
+        {
+            Console.WriteLine("ShowtoWindow method called.");
+        }
+
+        private void ShowToConsole()
+        {
+            Console.WriteLine("ShowToConsole Method called.");
+        }
+    }
+}
---
+++ VitalFeatures/VitalFeatures/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VitalFeatures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DelegateMe delegateme = new DelegateMe();
+            delegateme.BindMethodToDelegate();
+            Console.ReadLine();
+        } 
+    }
+}
